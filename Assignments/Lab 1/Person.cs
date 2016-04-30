1d2ccc77fd21ed5a95abd3ad752b533c5202365a
LaurenMazzoli <mazzolilauren@gmail.com>
2016-04-29 22:25:31 -0400

    rewrote lab 1

---
+++ Assignments/Lab 1/Person.cs
@@ -0,0 +1,68 @@
+﻿namespace Lab_1
+{
+
+    public class Person
+    {
+        public string FirstName;
+        public string MiddleInitial;
+        public string LastName;
+        public int Age;
+        public bool IsCitizen;
+        public double TotalHeightCM;
+
+        public void GetDataFromUser()
+        { // ** begin GetDataFromUser method **
+
+            // ** get name from user **
+            System.Console.Write("First Name: ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Middle Initial: ");
+            this.MiddleInitial = System.Console.ReadLine();
+
+            System.Console.Write("Last Name: ");
+            this.LastName = System.Console.ReadLine();
+
+            // ** get height from user **
+            System.Console.Write("Height (feet only): ");
+            int heightFeet = int.Parse(System.Console.ReadLine());
+
+            System.Console.Write("Height (inches only): ");
+            double heightInches = double.Parse(System.Console.ReadLine());
+
+            // ** calculate height in Inches and convert to Centimeters **
+            double totalHeightIN = heightFeet * 12 + heightInches;
+            this.TotalHeightCM = totalHeightIN * 2.54;
+
+            // ** get age from user **
+            System.Console.Write("Age: ");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            // ** get US Citizenship status from user **
+            System.Console.Write("Is " + this.FirstName + " a US Citizen (Y/N)? ");
+            string input = System.Console.ReadLine().ToLower();
+
+            if (input.StartsWith("y"))
+            {
+                this.IsCitizen = true;
+            }
+        } // end GetDataFromUser method
+
+        public string GetFullName()
+        { // ** begin GetFullName method **
+
+            string fullName;
+
+            fullName = this.FirstName;
+            fullName += " ";
+            if (this.MiddleInitial != "")
+            {
+                fullName += this.MiddleInitial;
+                fullName += ". ";
+            }
+            fullName += this.LastName;
+
+            return fullName;
+        } // ** end GetFullName method
+    } // ** end of class Person
+} // ** end of Lab1
---
+++ Assignments/Lab 1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab_1
+{
+    class Program
+    { // ** begin class Program **
+        static void Main(string[] args)
+        { // ** begin Main method**
+
+            // ** instansiate a new object of type Person  **   
+            Person newPerson = new Person();
+
+            // ** invoke void public method GetDataFromUser on class Person to populate member variabes of newPerson **
+            newPerson.GetDataFromUser();
+
+            // ** determine if the newPerson can vote **
+            bool canVote;
+            if (newPerson.IsCitizen && newPerson.Age > 18)
+            {
+                canVote = true;
+            }
+            else
+            {
+                canVote = false;
+            }
+
+            //** invoke PrintResults static void method on class Program to print newPerson data to Console **
+            PrintResults(newPerson, canVote);
+
+        } // ** end Main method **
+
+        static void PrintResults(Person NewPerson, bool CanVote)
+        {
+            //** call method GetFullName on class Person to return a formatted name **
+            System.Console.WriteLine();
+            System.Console.WriteLine("*******************************");
+            System.Console.WriteLine("Name entered: " + NewPerson.GetFullName());
+            System.Console.WriteLine("Height (CM): " + NewPerson.TotalHeightCM);
+            string prompt = " is ";
+            if (CanVote == false)
+            {
+                prompt += "not ";
+            }
+
+            prompt += "eligible to vote.";
+            System.Console.WriteLine(NewPerson.FirstName + prompt);
+            System.Console.ReadKey();
+        }
+    }
+}
