4501e6dbb2ce2816236202c6845a3cdbfc07f798
LaurenMazzoli <mazzolilauren@gmail.com>
2016-04-30 21:30:04 -0400

    Updated Lab 2 Rev

--- Assignments/Lab 2 V2/Program.cs
+++ Assignments/Lab 2 V2/Program.cs
@@ -7,46 +7,53 @@ namespace Lab_2_V2
         {
 
             Person p1 = new Person();
+            GetData(p1);
+
             Person p2 = new Person();
+            GetData(p2);
 
-            bool isMarried = false;
-            //string prompt = null
-
-            isMarried = p1.GetPersonData(null);
-            if (isMarried)
-            {
-                p1.Spouse = new Person();
-                p1.Spouse.Spouse = p1;
-                p1.Spouse.LastName = p1.LastName;
-                p1.Spouse.GetPersonData("Spouse ");
-            }
-            System.Console.WriteLine();
-
-            isMarried = p2.GetPersonData(null);
-            if (isMarried)
-            {
-                p2.Spouse = new Person();
-                p2.Spouse.Spouse = p2;
-                p2.Spouse.LastName = p2.LastName;
-                p2.Spouse.GetPersonData("Spouse ");
-            }
-            System.Console.WriteLine();
             System.Console.WriteLine("+++++++++++++ Results +++++++++++++++");
-            p1.PrintNameAndAge("");
-            if (p1.Spouse != null)
+            PrintData(p1);
+            PrintData(p2);
+            PrintAvgAge();
+
+            // ** calculate and print average age **
+
+
+
+        } // end main
+
+        public static void GetData(Person p)
+        {
+            bool isMarried = false;
+
+            isMarried = p.GetPersonData(null);
+            if (isMarried)
             {
-                p1.Spouse.PrintNameAndAge("Spouse ");
+                p.Spouse = new Person();
+                p.Spouse.Spouse = p;
+                p.Spouse.LastName = p.LastName;
+                p.Spouse.GetPersonData("Spouse ");
             }
             System.Console.WriteLine();
-            p2.PrintNameAndAge("");
+        } // end GetSpouseData
 
-            if (p2.Spouse != null)
+        public static void PrintData(Person p)
+        {
+            p.PrintNameAndAge("");
+            if (p.Spouse != null)
             {
-                p2.Spouse.PrintNameAndAge("Spouse ");
+                p.Spouse.PrintNameAndAge("Spouse ");
             }
-            double averageAge = Person.SumOfAllAges / Person.NbrPeople;
-            System.Console.Write("Average Age: " + averageAge);
+            System.Console.WriteLine();
+        } // end PrintData Method
+
+        public static void PrintAvgAge()
+        {
+            double avgAge = Person.SumOfAllAges / Person.NbrPeople;
+            System.Console.WriteLine("Average Age: " + avgAge);
             System.Console.ReadLine();
-        } // end main
+
+        } // end PrintAvg method
     } 
 }
