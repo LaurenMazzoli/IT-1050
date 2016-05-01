d5f1c0f1c6e09a7cc87e0d35fa334be76a63b2e4
LaurenMazzoli <mazzolilauren@gmail.com>
2016-04-30 20:43:29 -0400

    Lab 2 Version 2

---
+++ Assignments/Lab 2 V2/Person.cs
@@ -0,0 +1,70 @@
+﻿
+
+namespace Lab_2_V2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+        public static int NbrPeople;
+
+        public Person()
+        {
+            Person.NbrPeople++;
+            this.Spouse =null;
+        }
+
+        public bool GetPersonData(string prompt)
+        {
+
+            System.Console.Write(prompt + "First Name: ");
+            this.FirstName = System.Console.ReadLine();
+            if (this.Spouse == null)
+            {
+                System.Console.Write("Last Name: ");
+                this.LastName = System.Console.ReadLine();
+            }
+            System.Console.Write(prompt+"Age: ");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            SumOfAllAges += this.Age;
+
+            bool isMarried = false;
+
+            if (this.Spouse == null)
+            {
+                isMarried=GetMarriageStatus();
+            }
+            return isMarried;
+        }
+
+        public bool GetMarriageStatus()
+        {
+            bool status = false;
+            System.Console.Write("Married (Y/N): ");
+            string input = System.Console.ReadLine().ToLower();
+
+            if (input.StartsWith("y"))
+            {
+                status = true;
+            }
+            return status;
+        }
+
+        public string GetFullName()
+        {
+        return  this.FirstName+ " "+ this.LastName;
+        }
+
+        public void PrintNameAndAge(string Prompt)
+        {
+            System.Console.WriteLine(Prompt + "Name: " + GetFullName());
+            System.Console.WriteLine(Prompt+ "Age: " + this.Age);
+
+        }
+     }
+}
---
+++ Assignments/Lab 2 V2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Lab_2_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            bool isMarried = false;
+            //string prompt = null
+
+            isMarried = p1.GetPersonData(null);
+            if (isMarried)
+            {
+                p1.Spouse = new Person();
+                p1.Spouse.Spouse = p1;
+                p1.Spouse.LastName = p1.LastName;
+                p1.Spouse.GetPersonData("Spouse ");
+            }
+            System.Console.WriteLine();
+
+            isMarried = p2.GetPersonData(null);
+            if (isMarried)
+            {
+                p2.Spouse = new Person();
+                p2.Spouse.Spouse = p2;
+                p2.Spouse.LastName = p2.LastName;
+                p2.Spouse.GetPersonData("Spouse ");
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("+++++++++++++ Results +++++++++++++++");
+            p1.PrintNameAndAge("");
+            if (p1.Spouse != null)
+            {
+                p1.Spouse.PrintNameAndAge("Spouse ");
+            }
+            System.Console.WriteLine();
+            p2.PrintNameAndAge("");
+
+            if (p2.Spouse != null)
+            {
+                p2.Spouse.PrintNameAndAge("Spouse ");
+            }
+            double averageAge = Person.SumOfAllAges / Person.NbrPeople;
+            System.Console.Write("Average Age: " + averageAge);
+            System.Console.ReadLine();
+        } // end main
+    } 
+}
